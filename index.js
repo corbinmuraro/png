2cd95fecdf281b5e51c4ce01a077596b9e50187f
corbin muraro <corbinmuraro@gmail.com>
2020-10-15 16:58:31 -0600

    initial commit

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+const simpleParser = require("mailparser").simpleParser;
+const cheerio = require("cheerio");
+const AWS = require("aws-sdk");
+AWS.config.update({ region: "us-west-2" });
+const s3 = new AWS.S3();
+
+const main = async (message) => {
+  const { from, subject, attachments } = await simpleParser(message);
+  $ = cheerio.load(html);
+  let response = null;
+
+  if (subject == "png" && from.indexOf("corbinmuraro") > -1) {
+    console.log("found one");
+  }
+  else {
+    console.log("nothing found");
+  }
+};
+
+const res = await main();
+
 exports.handler = async (event) => {
     const response = {
         statusCode: 200,
